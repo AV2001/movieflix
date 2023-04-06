e153d8a4c5973675d46cafc925705a543e6730d8
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-04-06 11:37:58 +0400

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
     <div>
         <CssBaseline />
-        <h1>Movieflix</h1>
+        <main>
+            <Routes>
+                <Route path="/movie/:id" element={<h1>Movie Information</h1>} />
+                <Route path="/actors/:id" element={<h1>Actors</h1>} />
+                <Route path="/" element={<h1>Movies</h1>} />
+                <Route path="/profile/:id" element={<h1>Profile</h1>} />
+            </Routes>
+        </main>
     </div>
-)
+);
 
 export default App;
